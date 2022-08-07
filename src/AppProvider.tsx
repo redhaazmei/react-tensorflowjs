48ab182d5cd5c8a49672e4c3244393d0cdd44b8b
Redha Azmei <redha.azmei@gmail.com>
2022-08-07 19:36:10 +0800

    Create AppProvider

---
+++ src/AppProvider.tsx
@@ -0,0 +1,43 @@
+import { BrowserRouter } from 'react-router-dom';
+import {
+  ColorSchemeProvider,
+  MantineProvider,
+  ColorScheme,
+} from '@mantine/core';
+import { useLocalStorage } from '@mantine/hooks';
+
+interface AppProviderProps {
+  children: React.ReactNode;
+}
+
+export default function AppProvider({ children }: AppProviderProps) {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme,
+          fontFamily: 'Inter, sans-serif',
+          headings: {
+            fontFamily: 'Inter, sans-serif',
+          },
+        }}
+      >
+        <BrowserRouter>{children}</BrowserRouter>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+}
